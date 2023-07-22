6ac00a59df6e081070dd886d2e603ff2bd2f6b91
domnantas <hello@domnantas.lt>
2023-07-22 19:17:59 +0300

    Copy Vue blog

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import { genFeed } from "./genFeed.js";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Vilniaus Universiteto radijo klubas",
-  description: "Vilniaus Universiteto radijo klubo tinkaraštis",
+  title: "VURK",
+  description: "Vilniaus universiteto radijo klubo tinklaraštis",
+  cleanUrls: true,
+  buildEnd: genFeed,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+    // nav: [
+    //   { text: "Home", link: "/" },
+    //   { text: "Examples", link: "/markdown-examples" },
+    // ],
+    // sidebar: [
+    //   {
+    //     text: "Examples",
+    //     items: [
+    //       { text: "Markdown Examples", link: "/markdown-examples" },
+    //       { text: "Runtime API Examples", link: "/api-examples" },
+    //     ],
+    //   },
+    // ],
+    // socialLinks: [
+    //   { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    // ],
+  },
+});
