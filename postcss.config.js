eef909bd8def6639f195edd7f4619091f6f003f2
domnantas <hello@domnantas.lt>
2023-07-24 23:49:47 +0300

    Fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-import tailwind from "tailwindcss";
-import tailwindTypography from "@tailwindcss/typography";
-
 export default {
-	plugins: [
-		tailwind({
-			content: ["./.vitepress/theme/**/*.vue"],
-			plugins: [tailwindTypography],
-		}),
-	],
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+	},
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: [".vitepress/**/*.{vue,js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+	darkMode: "class",
 };
